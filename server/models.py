74ac0522356d655437d2146bba9e30b360ff5ba6
Collin Sakuma <collinmsakuma@gmail.com>
2023-10-17 14:17:57 -0700

    updated route model, addest steps

--- server/models.py
+++ server/models.py
@@ -9,30 +9,42 @@ class Route(db.Model):
     __tablename__ = "route"
 
     id = db.Column(db.Integer, primary_key=True)
-    route_code = db.Column(db.Integer)
-    route_path = db.Column(db.String)
+    tool_type = db.Column(db.String)
+    coating_type = db.Column(db.String)
+    step_1 = db.Column(db.String)
+    step_2 = db.Column(db.String)
+    step_3 = db.Column(db.String)
+    step_4 = db.Column(db.String)
+    step_5 = db.Column(db.String)
+    step_6 = db.Column(db.String)
+    step_7 = db.Column(db.String)
+    step_8 = db.Column(db.String)
+    step_9 = db.Column(db.String)
+    step_10 = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     def __repr__(self):
         return f'<{self.id}>'
     
-class Orders(db.Model):
-    __tablename__ = "orders"
+# class Orders(db.Model):
+#     __tablename__ = "orders"
 
-    id = db.Column(db.Integer, primary_key=True)
-    item_type = db.Column(db.String)
-    quantity = db.Column(db.Integer)
-    route = db.Column() # assosiation from routes
-    total_price = db.Column(db.Integer)
-    total_cost = db.Column(db.Integer)
+#     id = db.Column(db.Integer, primary_key=True)
+#     item_type = db.Column(db.String)
+#     quantity = db.Column(db.Integer)
+#     route = db.Column() # assosiation from routes
+#     total_price = db.Column(db.Integer)
+#     total_cost = db.Column(db.Integer)
 
-class Machines(db.Model):
-    __tablename__ = "machines"
+# class Machines(db.Model):
+#     __tablename__ = "machines"
 
-    id = db.Column(db.Integer, primary_key=True)
-    machine_name = db.Column(db.String)
+#     id = db.Column(db.Integer, primary_key=True)
+#     machine_name = db.Column(db.String)
 
-class Workers(db.Model):
-    __tablename = "workers"
+# class Workers(db.Model):
+#     __tablename = "workers"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
